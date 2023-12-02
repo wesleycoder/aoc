777a6c80eda562bf48a1421413d1e550d0dd1b61
Wésley Queiroz <wesley.coder@gmail.com>
2023-12-02 20:59:37 -0300

    test: fix test values for day 2

--- 2023/2/part2/index.test.ts
+++ 2023/2/part2/index.test.ts
@@ -4,9 +4,9 @@ import run from './'
 
 describe('2023/2 part2', () => {
   it('should return correctly for sample input', async () => {
-    expect(await run(resolve(__dirname, 'sample.txt'))).toEqual(8)
+    expect(await run(resolve(__dirname, 'sample.txt'))).toEqual(2286)
   })
   it('should return correctly for main input', async () => {
-    expect(await run(resolve(__dirname, '../input.txt'))).toEqual(1853)
+    expect(await run(resolve(__dirname, '../input.txt'))).toEqual(72706)
   })
 })
